61af170a31a4005a53cd755989b61319115ba9e7
carapaceonbear <seerhan17@gmail.com>
2022-05-27 16:31:33 +0100

    pieces spawn from code, using objects. Event listeners not yet working

--- app.js
+++ app.js
@@ -3,8 +3,39 @@ const gameBoard = document.querySelector(".main__board");
 
 
 const setUpBoard = () => {
+    spawnPiece("white_rook", "(0,0)");
+    spawnPiece("white_knight", "(1,0)");
+    spawnPiece("white_bishop", "(2,0)");
+    spawnPiece("white_queen", "(3,0)");
     spawnPiece("white_king", "(4,0)");
-    spawnPiece("black_king", "(3,7)")
+    spawnPiece("white_bishop", "(5,0)");
+    spawnPiece("white_knight", "(6,0)");
+    spawnPiece("white_rook", "(7,0)");
+    spawnPiece("white_pawn", "(0,1)");
+    spawnPiece("white_pawn", "(1,1)");
+    spawnPiece("white_pawn", "(2,1)");
+    spawnPiece("white_pawn", "(3,1)");
+    spawnPiece("white_pawn", "(4,1)");
+    spawnPiece("white_pawn", "(5,1)");
+    spawnPiece("white_pawn", "(6,1)");
+    spawnPiece("white_pawn", "(7,1)");
+
+    spawnPiece("black_rook", "(0,7)");
+    spawnPiece("black_knight", "(1,7)");
+    spawnPiece("black_bishop", "(2,7)");
+    spawnPiece("black_queen", "(3,7)");
+    spawnPiece("black_king", "(4,7)");
+    spawnPiece("black_bishop", "(5,7)");
+    spawnPiece("black_knight", "(6,7)");
+    spawnPiece("black_rook", "(7,7)");
+    spawnPiece("black_pawn", "(0,6)");
+    spawnPiece("black_pawn", "(1,6)");
+    spawnPiece("black_pawn", "(2,6)");
+    spawnPiece("black_pawn", "(3,6)");
+    spawnPiece("black_pawn", "(4,6)");
+    spawnPiece("black_pawn", "(5,6)");
+    spawnPiece("black_pawn", "(6,6)");
+    spawnPiece("black_pawn", "(7,6)");
 }
 
 const spawnPiece = (piece, xy) => {
