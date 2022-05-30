c6e8e14b33a7ef96f3c4ebd7bf161e5dc51f421d
carapaceonbear <seerhan17@gmail.com>
2022-05-30 22:48:50 +0100

    nicer looking pieces, with attribution link

--- app.js
+++ app.js
@@ -13,224 +13,224 @@ const pieces = [
         colour: "white",
         square: [0,0],
         ruleset: "rook",
-        image: "chess_piece_2_white_rook.png"
+        image: "w_rook_svg_withShadow.svg"
     },
     {
         name: "whiteKnightOne",
         colour: "white",
         square: [1,0],
         ruleset: "knight",
-        image: "chess_piece_2_white_knight.png"
+        image: "w_knight_svg_withShadow.svg"
     },
     {
         name: "whiteBishopOne",
         colour: "white",
         square: [2,0],
         ruleset: "bishop",
-        image: "chess_piece_2_white_bishop.png"
+        image: "w_bishop_svg_withShadow.svg"
     },
     {
         name: "whiteQueen",
         colour: "white",
         square: [3,0],
         ruleset: "queen",
-        image: "chess_piece_2_white_queen.png"
+        image: "w_queen_svg_withShadow.svg"
     },
     {
         name: "whiteKing",
         colour: "white",
         square: [4,0],
         ruleset: "king",
-        image: "chess_piece_2_white_king.png"
+        image: "w_king_svg_withShadow.svg"
     },
     {
         name: "whiteBishopTwo",
         colour: "white",
         square: [5,0],
         ruleset: "bishop",
-        image: "chess_piece_2_white_bishop.png"
+        image: "w_bishop_svg_withShadow.svg"
     },
     {
         name: "whiteKnightTwo",
         colour: "white",
         square: [6,0],
         ruleset: "knight",
-        image: "chess_piece_2_white_knight.png"
+        image: "w_knight_svg_withShadow.svg"
     },
     {
         name: "whiteRookTwo",
         colour: "white",
         square: [7,0],
         ruleset: "rook",
-        image: "chess_piece_2_white_rook.png"
+        image: "w_rook_svg_withShadow.svg"
     },
     {
         name: "whitePawnOne",
         colour: "white",
         square: [0,1],
         ruleset: "pawn",
-        image: "chess_piece_2_white_pawn.png"
+        image: "w_pawn_svg_withShadow.svg"
     },
     {
         name: "whitePawnTwo",
         colour: "white",
         square: [1,1],
         ruleset: "pawn",
-        image: "chess_piece_2_white_pawn.png"
+        image: "w_pawn_svg_withShadow.svg"
     },
     {
         name: "whitePawnThree",
         colour: "white",
         square: [2,1],
         ruleset: "pawn",
-        image: "chess_piece_2_white_pawn.png"
+        image: "w_pawn_svg_withShadow.svg"
     },
     {
         name: "whitePawnFour",
         colour: "white",
         square: [3,1],
         ruleset: "pawn",
-        image: "chess_piece_2_white_pawn.png"
+        image: "w_pawn_svg_withShadow.svg"
     },
     {
         name: "whitePawnFive",
         colour: "white",
         square: [4,1],
         ruleset: "pawn",
-        image: "chess_piece_2_white_pawn.png"
+        image: "w_pawn_svg_withShadow.svg"
     },
     {
         name: "whitePawnSix",
         colour: "white",
         square: [5,1],
         ruleset: "pawn",
-        image: "chess_piece_2_white_pawn.png"
+        image: "w_pawn_svg_withShadow.svg"
     },
     {
         name: "whitePawnSeven",
         colour: "white",
         square: [6,1],
         ruleset: "pawn",
-        image: "chess_piece_2_white_pawn.png"
+        image: "w_pawn_svg_withShadow.svg"
     },
     {
         name: "whitePawnEight",
         colour: "white",
         square: [7,1],
         ruleset: "pawn",
-        image: "chess_piece_2_white_pawn.png"
+        image: "w_pawn_svg_withShadow.svg"
     },
     {
         name: "blackRookOne",
         colour: "black",
         square: [0,7],
         ruleset: "rook",
-        image: "chess_piece_2_black_rook.png"
+        image: "b_rook_svg_withShadow.svg"
     },
     {
         name: "blackKnightOne",
         colour: "black",
         square: [1,7],
         ruleset: "knight",
-        image: "chess_piece_2_black_knight.png"
+        image: "b_knight_svg_withShadow.svg"
     },
     {
         name: "blackBishopOne",
         colour: "black",
         square: [2,7],
         ruleset: "bishop",
-        image: "chess_piece_2_black_bishop.png"
+        image: "b_bishop_svg_withShadow.svg"
     },
     {
         name: "blackQueen",
         colour: "black",
         square: [3,7],
         ruleset: "queen",
-        image: "chess_piece_2_black_queen.png"
+        image: "b_queen_svg_withShadow.svg"
     },
     {
         name: "blackKing",
         colour: "black",
         square: [4,7],
         ruleset: "king",
-        image: "chess_piece_2_black_king.png"
+        image: "b_king_svg_withShadow.svg"
     },
     {
         name: "blackBishopTwo",
         colour: "black",
         square: [5,7],
         ruleset: "bishop",
-        image: "chess_piece_2_black_bishop.png"
+        image: "b_bishop_svg_withShadow.svg"
     },
     {
         name: "blackKnightTwo",
         colour: "black",
         square: [6,7],
         ruleset: "knight",
-        image: "chess_piece_2_black_knight.png"
+        image: "b_knight_svg_withShadow.svg"
     },
     {
         name: "blackRookTwo",
         colour: "black",
         square: [7,7],
         ruleset: "rook",
-        image: "chess_piece_2_black_rook.png"
+        image: "b_rook_svg_withShadow.svg"
     },
     {
         name: "blackPawnOne",
         colour: "black",
         square: [0,6],
         ruleset: "pawn",
-        image: "chess_piece_2_black_pawn.png"
+        image: "b_pawn_svg_withShadow.svg"
     },
     {
         name: "blackPawnTwo",
         colour: "black",
         square: [1,6],
         ruleset: "pawn",
-        image: "chess_piece_2_black_pawn.png"
+        image: "b_pawn_svg_withShadow.svg"
     },
     {
         name: "blackPawnThree",
         colour: "black",
         square: [2,6],
         ruleset: "pawn",
-        image: "chess_piece_2_black_pawn.png"
+        image: "b_pawn_svg_withShadow.svg"
     },
     {
         name: "blackPawnFour",
         colour: "black",
         square: [3,6],
         ruleset: "pawn",
-        image: "chess_piece_2_black_pawn.png"
+        image: "b_pawn_svg_withShadow.svg"
     },
     {
         name: "blackPawnFive",
         colour: "black",
         square: [4,6],
         ruleset: "pawn",
-        image: "chess_piece_2_black_pawn.png"
+        image: "b_pawn_svg_withShadow.svg"
     },
     {
         name: "blackPawnSix",
         colour: "black",
         square: [5,6],
         ruleset: "pawn",
-        image: "chess_piece_2_black_pawn.png"
+        image: "b_pawn_svg_withShadow.svg"
     },
     {
         name: "blackPawnSeven",
         colour: "black",
         square: [6,6],
         ruleset: "pawn",
-        image: "chess_piece_2_black_pawn.png"
+        image: "b_pawn_svg_withShadow.svg"
     },
     {
         name: "blackPawnEight",
         colour: "black",
         square: [7,6],
         ruleset: "pawn",
-        image: "chess_piece_2_black_pawn.png"
+        image: "b_pawn_svg_withShadow.svg"
     }
 ];
 
